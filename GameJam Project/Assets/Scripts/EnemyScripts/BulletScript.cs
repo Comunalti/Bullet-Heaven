b8d2750aa6b04ae80069f28ca7a6667d4957bf94
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-01-26 18:20:40 -0300

    Coloquei o bouncing no material da bala

--- GameJam Project/Assets/Scripts/EnemyScripts/BulletScript.cs
+++ GameJam Project/Assets/Scripts/EnemyScripts/BulletScript.cs
@@ -1,9 +1,15 @@
-using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class BulletScript : MonoBehaviour{
-   private void OnCollisionEnter2D(Collision2D other) {
+   private void OnBecameInvisible() {
       Destroy(gameObject);
+   }
+
+   private void OnCollisionEnter2D(Collision2D other) {
+      if (other.gameObject.tag != "Plataforma") 
+         Destroy(gameObject);
+      
+        
    }
 }
