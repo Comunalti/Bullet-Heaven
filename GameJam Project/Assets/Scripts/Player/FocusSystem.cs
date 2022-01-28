7c825942fea689e57a1180deafaea8d0228d91ff
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-01-28 19:58:58 -0300

    sistema de foco

---
+++ GameJam Project/Assets/Scripts/Player/FocusSystem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Player{
+    public class FocusSystem : MonoBehaviour{
+        [SerializeField] private float currentFocus; //esse serialize field é só pra facilitar o debug
+        [SerializeField] private float initialFocus;
+        [SerializeField] private float gainFocusPerSecond;
+
+        private void Awake() {
+            currentFocus = initialFocus;
+        }
+
+        private void Update() {
+            currentFocus += gainFocusPerSecond * Time.deltaTime;
+        }
+
+        public float CurrentFocus {
+            get => currentFocus;
+            set {
+                if (currentFocus < 0)
+                    currentFocus = 0;
+               
+                else
+                    currentFocus = value;
+            }
+        }
+    }
+}
