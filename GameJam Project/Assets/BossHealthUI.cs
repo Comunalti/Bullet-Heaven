d9f3c4f9ba0b60a89749f25e87c27524130de248
Comunalti <frps2003@gmail.com>
2022-01-30 17:05:42 -0300

    barra de boss

---
+++ GameJam Project/Assets/BossHealthUI.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using Entity;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealthUI : MonoBehaviour
+{
+    private Image _image;
+    [SerializeField] private Health _health;
+    void Start()
+    {
+        _image = GetComponent<Image>();
+        _health.OnDiedEvent += OnDied;
+        _health.OnHealthRemovedEvent += OnDamage;
+        _health.OnHealthAddedEvent += OnDamage;
+    }
+
+    private void OnDestroy()
+    {
+        _health.OnDiedEvent -= OnDied;
+        _health.OnHealthRemovedEvent -= OnDamage;
+        _health.OnHealthAddedEvent -= OnDamage;
+    }
+
+
+    private void OnDied()
+    {
+       Destroy(gameObject.transform.parent.gameObject);
+       Destroy(_health.gameObject);
+       GlobalEvents.WinGame?.Invoke();
+    }
+
+    private void OnDamage()
+    {
+        _image.fillAmount = _health.CurrentHealth / _health.MaxHealth;
+    }
+}
