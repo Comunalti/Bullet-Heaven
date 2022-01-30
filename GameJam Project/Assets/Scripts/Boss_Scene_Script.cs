8cc4bd125facd25d5b6755b487ee93e625c1a9a2
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-01-30 17:46:13 -0300

    Yay yay

---
+++ GameJam Project/Assets/Scripts/Boss_Scene_Script.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Boss_Scene_Script : MonoBehaviour{
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Boss_Scene")
+            SceneManager.LoadScene("Boss_scene");
+    }
+}
