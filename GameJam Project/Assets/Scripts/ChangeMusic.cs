1b80c8666906ae9524bcd72f0b442e59c023d7a6
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-01-30 22:17:57 -0300

    Music changer adicionado

---
+++ GameJam Project/Assets/Scripts/ChangeMusic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace{
+    public class ChangeMusic : MonoBehaviour{
+        [SerializeField] private AudioClip _nextSong;
+        private Camera _camera;
+        private AudioSource _audioSource;
+
+        private void Start() {
+            _camera = Camera.main;
+            _audioSource = _camera.GetComponent<AudioSource>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            if (other.gameObject.tag == "Player") {
+                _audioSource.clip = _nextSong;
+                _audioSource.Play();
+            }
+        }
+        
+    }
+}
